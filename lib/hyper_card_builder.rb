b1041ffbc402d7863c1555398452f051805a32de
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2025-11-23 14:22:12 +0900

    before all thor test and exec methods

---
+++ lib/hyper_card_builder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hyper_card_builder/version"
+
+module HyperCardBuilder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "hyper_card_builder"
+
+require "minitest/autorun"
